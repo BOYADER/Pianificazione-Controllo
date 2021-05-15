7547b78d5b9a5f7439a3b4b67ca042c671f543b1
marghe-b <marghe.bongiorni@gmail.com>
2021-05-15 20:57:37 +0200

    nodo publish Odom.msg

--- pc_wp/scripts/navigation.py
+++ pc_wp/scripts/navigation.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import Float64
+from pc_wp.msg import Odom
 
 def navigation():
-	pub = rospy.Publisher('odom', Float64, queue_size=10)
+	pub = rospy.Publisher('odom', Odom, queue_size=10)
 	rospy.init_node('navigation')
-	rate = rospy.Rate(1) # 1hz
-	num = 1
+	rate = rospy.Rate(10) # 10hz
+	odom_msg = Odom()
+	odom_msg.lla.x = 1
+	odom_msg.lla.y = 2
+	odom_msg.lla.z = 3
+	odom_msg.rpy.x = 30
+	odom_msg.rpy.y = 60
+	odom_msg.rpy.z = 90
+	odom_msg.lin_vel.x = 1.5
+	odom_msg.lin_vel.y = 0.1
+	odom_msg.lin_vel.z = 0.1
 	while not rospy.is_shutdown():
-		rospy.loginfo(num)
-		pub.publish(num)
-		num = num + 1
+		rospy.loginfo(odom_msg)
+		pub.publish(odom_msg)
 		rate.sleep()
 
 if __name__ == '__main__':
