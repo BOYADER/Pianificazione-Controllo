7dcc94279269116a12be45f2cbc3026ef8452879
marghe-b <marghe.bongiorni@gmail.com>
2021-05-17 15:44:07 +0200

    correzione errori

--- pc_wp/scripts/classes/AUV.py
+++ pc_wp/scripts/classes/AUV.py
@@ -2,7 +2,7 @@
 import math
 import numpy as np
 import rospy
-import Waypoint
+from Waypoint import Waypoint
 
 class AUV:
 	def __init__(self, latitude, longitude, depth, roll, pitch, yaw, vx, vy, vz, critical_pitch):
