bc844559cac456ffb4721bbeedd19a2f638247e3
marghe-b <marghe.bongiorni@gmail.com>
2021-05-17 17:07:24 +0200

    funzione geodetic2ned

--- pc_wp/scripts/classes/AUV.py
+++ pc_wp/scripts/classes/AUV.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import math
 import numpy as np
+import pymap3d as pm
 import rospy
 from Waypoint import Waypoint
 
@@ -9,64 +10,25 @@ class AUV:
 		self.latitude = latitude
 		self.longitude = longitude
 		self.depth = depth
+		self.latitude_ned = latitude
+		self.longitude_ned = longitude
+		self.depth_ned = depth
 		self.roll = roll
 		self.pitch = pitch
 		self.yaw = yaw
 		self.critical_pitch = critical_pitch
-		self.x = None
-		self.y = None
-		self.z = None
+		self.x = pm.geodetic2ned(self.latitude, self.longitude, self.depth, self.latitude_ned, self.longitude_ned, self.depth_ned)[0]
+		self.y = pm.geodetic2ned(self.latitude, self.longitude, self.depth, self.latitude_ned, self.longitude_ned, self.depth_ned)[1]
+		self.z = pm.geodetic2ned(self.latitude, self.longitude, self.depth, self.latitude_ned, self.longitude_ned, self.depth_ned)[2]
 		self.vx = vx
 		self.vy = vy
 		self.vz = vz
-		self.latitude_ned = latitude
-		self.longitude_ned = longitude
-		self.depth_ned = depth
 		self.strategy = 0
 		self.task_seq = []
 		self.task_index = 0
 		self.waypoints = []
 		self.wp_index = 0
-		self.geo2ned(self.latitude, self.longitude, self.depth, -1)	
 		
-	def geo2ned(self, latitude, longitude, depth, index):
-		R_e = rospy.get_param('/R_e') 
-		R_p = rospy.get_param('/R_p')
-		e = math.sqrt(1 - (R_p*R_p)/(R_e*R_e))
-		N_e = R_e/(math.sqrt(1 - e*e*math.sin(latitude)*math.sin(latitude)))
-		x_ecef = (N_e - depth) * math.cos(latitude) * math.cos(longitude) 							# h = - depth
-		y_ecef = (N_e - depth) * math.cos(latitude) * math.sin(longitude)
-		z_ecef = (N_e * (1 - e*e) - depth) * math.sin(latitude)
-		coords_ecef = np.array([[x_ecef, y_ecef, z_ecef]]).T
-	
-		N_e_ned_frame = R_e/(math.sqrt(1 - e*e*math.sin(self.latitude_ned)*math.sin(self.latitude_ned)))
-		x_ecef_ned_frame = (N_e_ned_frame - self.depth_ned) * math.cos(self.latitude_ned) * math.cos(self.longitude_ned)
-		y_ecef_ned_frame = (N_e_ned_frame - self.depth_ned) * math.cos(self.latitude_ned) * math.sin(self.longitude_ned)
-		z_ecef_ned_frame = (N_e_ned_frame * (1 - e*e) - self.depth_ned) * math.sin(self.latitude_ned)
-		coords_ecef_ned_frame = np.array([[x_ecef_ned_frame,y_ecef_ned_frame, z_ecef_ned_frame]]).T
-
-		r11 = -(math.sin(self.latitude_ned) * math.cos(self.longitude_ned))
-		r12 = -(math.sin(self.latitude_ned) * math.sin(self.longitude_ned))
-		r13 = math.cos(self.latitude_ned)
-		r21 = -math.sin(self.longitude_ned)
-		r22 = math.cos(self.longitude_ned)
-		r23 = 0
-		r31 = -(math.cos(self.latitude_ned) * math.cos(self.longitude_ned))
-		r32 = -(math.cos(self.latitude_ned) * math.sin(self.longitude_ned))
-		r33 = -math.sin(self.latitude_ned)
-		R_ned_ecef = np.matrix([[r11, r12, r13],[r21, r22, r23],[r31, r32, r33]])
-
-		coords_ned = R_ned_ecef.dot(coords_ecef - coords_ecef_ned_frame)
-		
-		if index == -1:
-			self.x = coords_ned.item(0)
-			self.y = coords_ned.item(1)
-			self.z = coords_ned.item(2)
-		else:
-			self.waypoints[index].x = coords_ned.item(0)
-			self.waypoints[index].y = coords_ned.item(1)
-			self.waypoints[index].z = coords_ned.item(2)
-
 	def init_waypoints(self):
 		tolerance = rospy.get_param('/tolerance_on_waypoint')
 		index = 1
@@ -75,10 +37,8 @@ class AUV:
 			latitude = rospy.get_param(string_param)['latitude']
 			longitude = rospy.get_param(string_param)['longitude']
 			depth = rospy.get_param(string_param)['depth']
-			self.waypoints.append(Waypoint(latitude, longitude, depth, tolerance))
-			print(self.waypoints[index-1].latitude, self.waypoints[index-1].longitude, self.waypoints[index-1].depth)
-			self.geo2ned(latitude, longitude, depth, index-1)
-			print(self.waypoints[index-1].x, self.waypoints[index-1].y, self.waypoints[index-1].z)
+			self.waypoints.append(Waypoint(latitude, longitude, depth, self.latitude_ned, self.longitude_ned, self.depth_ned, tolerance))
+			print(self.waypoints[index-1].x,self.waypoints[index-1].y,self.waypoints[index-1].z)
 			index = index + 1	
 	
 	def update(self, latitude, longitude, depth, roll, pitch, yaw, vx, vy, vz):
@@ -91,7 +51,9 @@ class AUV:
 		self.vx = vx
 		self.vy = vy
 		self.vz = vz
-		self.geo2ned(self.latitude, self.longitude, self.depth, -1)
+		self.x = pm.geodetic2ned(self.latitude, self.longitude, self.depth, self.latitude_ned, self.longitude_ned, self.depth_ned)[0]
+		self.y = pm.geodetic2ned(self.latitude, self.longitude, self.depth, self.latitude_ned, self.longitude_ned, self.depth_ned)[1]
+		self.z = pm.geodetic2ned(self.latitude, self.longitude, self.depth, self.latitude_ned, self.longitude_ned, self.depth_ned)[2]
 	
 #def init_auv():
 #	global auv
--- pc_wp/scripts/classes/Waypoint.py
+++ pc_wp/scripts/classes/Waypoint.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
+import pymap3d as pm
 
 class Waypoint:
-	def __init__(self, latitude, longitude, depth, tolerance):
+	def __init__(self, latitude, longitude, depth, latitude_ned, longitude_ned, depth_ned, tolerance):
 		self.latitude = latitude
 		self.longitude = longitude
 		self.depth = depth
+		self.latitude_ned = latitude_ned
+		self.longitude_ned = longitude_ned
+		self.depth_ned = depth_ned
 		self.tolerance = tolerance
-		self.x = None
-		self.y = None
-		self.z = None
+		self.x = pm.geodetic2ned(self.latitude, self.longitude, self.depth, self.latitude_ned, self.longitude_ned, self.depth_ned)[0]
+		self.y = pm.geodetic2ned(self.latitude, self.longitude, self.depth, self.latitude_ned, self.longitude_ned, self.depth_ned)[1]
+		self.z = pm.geodetic2ned(self.latitude, self.longitude, self.depth, self.latitude_ned, self.longitude_ned, self.depth_ned)[2]
 
