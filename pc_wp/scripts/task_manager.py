4d4a8e7634b30d1c09dd89eed0fab1cedb15f97a
marghe-b <82581215+marghe-b@users.noreply.github.com>
2021-05-19 10:21:46 +0200

    Update task_manager.py

--- pc_wp/scripts/task_manager.py
+++ pc_wp/scripts/task_manager.py
@@ -11,34 +11,34 @@ references = References()
 def odom_callback(odom, pub):
 	global auv, references
 	if not auv:
-		auv = AUV(	odom.lla.x, odom.lla.y, odom.lla.z,	# create AUV object
+		auv = AUV(	odom.lla.x, odom.lla.y, odom.lla.z,		# create AUV object
 				odom.rpy.x, odom.rpy.y, odom.rpy.z,
 				odom.lin_vel.x, odom.lin_vel.y, odom.lin_vel.z)
-		auv.init_waypoints()														# init waypoint list
-		pitch_des = auv.pitch_desired()									# compute pitch_des to decide the strategy
-		auv.set_strategy(pitch_des)											# strategy determined, task_seq initialized
+		auv.init_waypoints()						# init waypoint list
+		pitch_des = auv.pitch_desired()					# compute pitch_des to decide the strategy
+		auv.set_strategy(pitch_des)					# strategy determined, task_seq initialized
 		print("pitch_des: %s" % str(pitch_des))
-		auv.set_strategy(pitch_des)											# set strategy and tasks_sequence
-		auv.set_tolerance()															# set task tolerance error 
+		auv.set_strategy(pitch_des)					# set strategy and tasks_sequence
+		auv.set_tolerance()						# set task tolerance error 
 	else:
-		auv.update(	odom.lla.x, odom.lla.y, odom.lla.z,	# update eta_1, eta_2, ni_1
+		auv.update(	odom.lla.x, odom.lla.y, odom.lla.z,		# update eta_1, eta_2, ni_1
 				odom.rpy.x, odom.rpy.y, odom.rpy.z,
 				odom.lin_vel.x, odom.lin_vel.y, odom.lin_vel.z)
-		task_error = auv.task_error(references)					# task_error computation
+		task_error = auv.task_error(references)				# task_error computation
 		print("%s error: %s" % (auv.task_seq[auv.task_index], str(abs(task_error))))		
-		if abs(task_error) < auv.tolerance:							# check if current task is completed or not
-			if auv.task_index == (len(auv.task_seq) - 1):	# waypoint approached
-				auv.wp_index = auv.wp_index + 1							# next waypoint, TODO: check waypoints list
-				pitch_des = auv.pitch_desired()							# compute pitch_des to decide the strategy
-				auv.set_strategy(pitch_des)									# strategy determined, task_seq initialized
+		if abs(task_error) < auv.tolerance:				# check if current task is completed or not
+			if auv.task_index == (len(auv.task_seq) - 1):		# waypoint approached
+				auv.wp_index = auv.wp_index + 1			# next waypoint, TODO: check waypoints list
+				pitch_des = auv.pitch_desired()			# compute pitch_des to decide the strategy
+				auv.set_strategy(pitch_des)			# strategy determined, task_seq initialized
 			else:																					# task completed, waypoint not yet approached
-				auv.task_index = auv.task_index + 1					# next task
-				auv.set_tolerance()													# update task tolerance
-	state = State()																		# prepare msg State to task_nodes
+				auv.task_index = auv.task_index + 1		# next task
+				auv.set_tolerance()				# update task tolerance
+	state = State()								# prepare msg State to task_nodes
 	state.strategy = auv.strategy
 	state.task = auv.task_seq[auv.task_index]
 	print(state)
-	pub.publish(state)																# msg published to topic
+	pub.publish(state)							# msg published to topic
 
 def ref_callback(ref):
 	global references
