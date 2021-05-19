3a4bc180935e3440298be7588fd0996a5917c859
marghe-b <82581215+marghe-b@users.noreply.github.com>
2021-05-19 10:18:53 +0200

    Update Waypoint.py

--- pc_wp/scripts/classes/Waypoint.py
+++ pc_wp/scripts/classes/Waypoint.py
@@ -6,6 +6,6 @@ class Waypoint:
 		self.lld = [latitude, longitude, depth]
 		self.lld_ned = [latitude_ned, longitude_ned, depth_ned]
 		self.eta_1 = [	pm.geodetic2ned(self.lld[0], self.lld[1], -self.lld[2], self.lld_ned[0], self.lld_ned[1], -self.lld_ned[2])[0],
-										pm.geodetic2ned(self.lld[0], self.lld[1], -self.lld[2], self.lld_ned[0], self.lld_ned[1], -self.lld_ned[2])[1],
-										pm.geodetic2ned(self.lld[0], self.lld[1], -self.lld[2], self.lld_ned[0], self.lld_ned[1], -self.lld_ned[2])[2]]
+				pm.geodetic2ned(self.lld[0], self.lld[1], -self.lld[2], self.lld_ned[0], self.lld_ned[1], -self.lld_ned[2])[1],
+				pm.geodetic2ned(self.lld[0], self.lld[1], -self.lld[2], self.lld_ned[0], self.lld_ned[1], -self.lld_ned[2])[2]]
 
