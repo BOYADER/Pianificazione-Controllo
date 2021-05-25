ab9d1452676e51d074b874a5c5949eebe8e7eda2
Lubern1 <82580905+Lubern1@users.noreply.github.com>
2021-05-25 12:17:17 +0200

    Update yaw_task.py

--- pc_wp/scripts/yaw_task.py
+++ pc_wp/scripts/yaw_task.py
@@ -7,8 +7,8 @@ from classes.Waypoint import Waypoint
 from pc_wp.msg import References, State, Odom
 from utils import get_waypoint, clear_vars
 
+#variabili globali
 waypoint = None
-
 eta_1 = []
 eta_2 = []
 eta_1_init = None
@@ -16,7 +16,8 @@ eta_2_init = None
 
 QUEUE_SIZE = rospy.get_param('/QUEUE_SIZE')
 
-def odom_callback(odom):
+#funzione che salva in eta_1 e eta_2 i valori attuali di posizione e orientazione dell'AUV e imposta eta_1_init e eta_2_init con i primi valori ricevuti all'attivazione di YAW task
+def odom_callback(odom): 
 	global eta_1, eta_2, eta_1_init, eta_2_init
 	lld_ned = rospy.get_param('ned_origin')
 	eta_1 = [	pm.geodetic2ned(odom.lld.x, odom.lld.y, -odom.lld.z, lld_ned['latitude'], lld_ned['longitude'], -lld_ned['depth'])[0], 
@@ -25,7 +26,7 @@ def odom_callback(odom):
 	eta_2 = [	odom.rpy.x,
 			odom.rpy.y,
 			odom.rpy.z]
-	if not eta_1_init:
+	if not eta_1_init:  
 		eta_1_init = eta_1
 	if not eta_2_init:
 		eta_2_init = eta_2
@@ -34,25 +35,28 @@ def odom_callback(odom):
 
 def state_callback(state, pub):
 	global waypoint, eta_1_init, eta_2_init
-	if state.task == 'YAW':
-		while eta_1_init is None or eta_2_init is None:
+	if state.task == 'YAW':  #se nel messaggio state il campo task è YAW
+		while eta_1_init is None or eta_2_init is None:  #attendo che vengano inizializzate
 				pass
 		pos_ref = eta_1_init
 		rpy_ref = eta_2_init	
-		if not waypoint:
+		if not waypoint: #inizializzo il waypoint da raggiungere
 			waypoint = get_waypoint(state.wp_index)
+		#riferimento di yaw
 		rpy_ref[2] = np.arctan2(	waypoint.eta_1[1] - eta_1[1],
 						waypoint.eta_1[0] - eta_1[0])
+		
+		#setto i riferimenti
 		references = References()
 		references.pos.x = pos_ref[0]
 		references.pos.y = pos_ref[1]
 		references.pos.z = pos_ref[2]
-		references.rpy.x = rpy_ref[0]
+		references.rpy.x = rpy_ref[0]  #roll non verrà usato 
 		references.rpy.y = rpy_ref[1]
 		references.rpy.z = rpy_ref[2]
 		print("YAW REFERENCE: %s" % int(round(math.degrees(rpy_ref[2]))))
 		pub.publish(references)
-	elif state.task != 'YAW':
+	elif state.task != 'YAW':  #se non sta al task YAW non viene pubblicato niente e setta le variabili a None nuovamente
 		[waypoint, eta_1_init, eta_2_init] = clear_vars([waypoint, eta_1_init, eta_2_init])
 
 def yaw_task():
