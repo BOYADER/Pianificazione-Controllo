b3e46738e1849908a311c730ddceb11dea8f74bf
FraFrusciante <82581034+FraFrusciante@users.noreply.github.com>
2021-05-26 11:24:58 +0200

    Update error _NED_body.py

--- pc_wp/scripts/error _NED_body.py
+++ pc_wp/scripts/error _NED_body.py
@@ -4,9 +4,10 @@ import numpy as np
 import math
 from pc_wp.msg import References, Odom
 
-global position_error_ned = np.array([references.pose.x - odom.lld.x, references.pose.y - odom.pose.y, references.pose.z - odom.pose.z])
-global pose_error = np.array([references.rpy.x - odom.rpy.x, references.rpy.y - odom.rpy.y, references.rpy.z - odom.rpy.z])
-global velocity_error = np.array([rospy.get_param('/surge_velocity') - odom.lin_vel.x, 0, 0]) 
+global position_error_ned, pose_error, velocity_error
+position_error_ned = np.array([references.pose.x - odom.lld.x, references.pose.y - odom.pose.y, references.pose.z - odom.pose.z])
+pose_error = np.array([references.rpy.x - odom.rpy.x, references.rpy.y - odom.rpy.y, references.rpy.z - odom.rpy.z])
+velocity_error = np.array([rospy.get_param('/surge_velocity') - odom.lin_vel.x, 0, 0]) 
 
 def error_body (position_error):
 #definisco J1(eta2)
